3a4a638568ae7141fb6bedc88a719cbc3ce4f741
jedlimlx <jedlimlx@outlook.com>
2023-09-16 22:57:37 +0800

    Fix issues with autoencoder

--- models/masked_sinogram_autoencoder.py
+++ models/masked_sinogram_autoencoder.py
@@ -6,13 +6,147 @@ import keras_core.ops as ops
 from keras_core.layers import *
 from keras_core.models import *
 
-from keras_cv.layers import TransformerEncoder
+# from keras_cv.layers import TransformerEncoder
 from keras_nlp.layers import PositionEmbedding
 
 from functools import partial
 
 from utils import add_noise
 from utils.data import process_sinogram
+
+
+class TransformerEncoder(Layer):  # todo use keras cv implementation when it becomes better
+    """
+    Transformer encoder block implementation as a Keras Layer.
+
+    Args:
+        project_dim: the dimensionality of the projection of the encoder, and
+            output of the `MultiHeadAttention`
+        mlp_dim: the intermediate dimensionality of the MLP head before
+            projecting to `project_dim`
+        num_heads: the number of heads for the `MultiHeadAttention` layer
+        mlp_dropout: default 0.1, the dropout rate to apply between the layers
+            of the MLP head of the encoder
+        attention_dropout: default 0.1, the dropout rate to apply in the
+            MultiHeadAttention layer
+        activation: default 'tf.activations.gelu', the activation function to
+            apply in the MLP head - should be a function
+        layer_norm_epsilon: default 1e-06, the epsilon for `LayerNormalization`
+            layers
+
+    Basic usage:
+
+    ```
+    project_dim = 1024
+    mlp_dim = 3072
+    num_heads = 4
+
+    encoded_patches = keras_cv.layers.PatchingAndEmbedding(
+        project_dim=project_dim,
+        patch_size=16)(img_batch)
+    trans_encoded = keras_cv.layers.TransformerEncoder(project_dim=project_dim,
+        mlp_dim = mlp_dim,
+        num_heads=num_heads)(encoded_patches)
+
+    print(trans_encoded.shape) # (1, 197, 1024)
+    ```
+    """
+
+    def __init__(
+            self,
+            project_dim,
+            num_heads,
+            mlp_dim,
+            mlp_dropout=0.1,
+            attention_dropout=0.1,
+            activation=kc.activations.gelu,
+            layer_norm_epsilon=1e-06,
+            **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.project_dim = project_dim
+        self.mlp_dim = mlp_dim
+        self.num_heads = num_heads
+        self.mlp_dropout = mlp_dropout
+        self.attention_dropout = attention_dropout
+        self.activation = activation
+        self.layer_norm_epsilon = layer_norm_epsilon
+        self.mlp_units = [mlp_dim, project_dim]
+
+        self.layer_norm1 = LayerNormalization(
+            epsilon=self.layer_norm_epsilon
+        )
+        self.layer_norm2 = LayerNormalization(
+            epsilon=self.layer_norm_epsilon
+        )
+        self.attn = MultiHeadAttention(
+            num_heads=self.num_heads,
+            key_dim=self.project_dim // self.num_heads,
+            dropout=self.attention_dropout,
+        )
+        self.dense1 = Dense(self.mlp_units[0])
+        self.dense2 = Dense(self.mlp_units[1])
+
+    def call(self, inputs):
+        """Calls the Transformer Encoder on an input sequence.
+        Args:
+            inputs: A `tf.Tensor` of shape [batch, height, width, channels]
+
+        Returns:
+            `A tf.Tensor` of shape [batch, patch_num+1, embedding_dim]
+        """
+
+        if inputs.shape[-1] != self.project_dim:
+            raise ValueError(
+                "The input and output dimensionality must be the same, but the "
+                f"TransformerEncoder was provided with {inputs.shape[-1]} and "
+                f"{self.project_dim}"
+            )
+
+        x = self.layer_norm1(inputs)
+        x = self.attn(x, x)
+        x = Dropout(self.mlp_dropout)(x)
+        x = Add()([x, inputs])
+
+        y = self.layer_norm2(x)
+
+        y = self.dense1(y)
+        if self.activation == kc.activations.gelu:
+            y = Activation(self.activation)(y, approximate=True)
+        else:
+            y = Activation(self.activation)(y)
+        y = Dropout(self.mlp_dropout)(y)
+        y = self.dense2(y)
+        y = Dropout(self.mlp_dropout)(y)
+
+        output = Add()([x, y])
+
+        return output
+
+    def get_config(self):
+        config = super().get_config()
+        activation = self.activation
+        if not isinstance(activation, (str, dict)):
+            activation = kc.activations.serialize(activation)
+        config.update(
+            {
+                "project_dim": self.project_dim,
+                "mlp_dim": self.mlp_dim,
+                "num_heads": self.num_heads,
+                "attention_dropout": self.attention_dropout,
+                "mlp_dropout": self.mlp_dropout,
+                "activation": activation,
+                "layer_norm_epsilon": self.layer_norm_epsilon,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config, custom_objects=None):
+        activation = config.pop("activation")
+        if isinstance(activation, (str, dict)):
+            activation = kc.activations.deserialize(activation)
+        return cls(activation=activation, **config)
 
 
 class Patches(Layer):
@@ -45,7 +179,8 @@ class Patches(Layer):
 
 
 class PatchDecoder(Layer):
-    def __init__(self, patch_width, patch_height, x_patches, y_patches, channels=1, ignore_last=False, name=None, **kwargs):
+    def __init__(self, patch_width, patch_height, x_patches, y_patches, channels=1, ignore_last=False, name=None,
+                 **kwargs):
         super(PatchDecoder, self).__init__(name=name, **kwargs)
         self.channels = channels
         self.patch_width = patch_width
@@ -59,7 +194,8 @@ class PatchDecoder(Layer):
         if self.ignore_last: encoded = encoded[:, :-1, :]
         reshaped = ops.reshape(encoded, (-1, self.y_patches, self.x_patches, self.patch_height, self.patch_width))
         reshaped = ops.transpose(reshaped, [0, 1, 3, 2, 4])
-        reshaped = ops.reshape(reshaped, (-1, self.y_patches * self.patch_height, self.x_patches * self.patch_width, self.channels))
+        reshaped = ops.reshape(reshaped, (
+        -1, self.y_patches * self.patch_height, self.x_patches * self.patch_width, self.channels))
 
         return reshaped
 
@@ -89,6 +225,8 @@ class SinogramPatchEncoder(Layer):
         self.projection_dim = projection_dim
         self.mask_proportion = mask_proportion
 
+        self.num_mask = int(num_patches * mask_proportion)
+
         # projection
         self.projection = Dense(units=projection_dim)
 
@@ -102,8 +240,11 @@ class SinogramPatchEncoder(Layer):
     def call(self, patch, mask_indices=None, unmask_indices=None, **kwargs):
         batch_size = ops.shape(patch)[0]
 
+        # encoding + positional embedding
         encoded = self.projection(patch)
-        embedding = self.position_embedding(patch)
+        embedding = self.position_embedding(encoded)
+
+        encoded = encoded + embedding
 
         if mask_indices is None or unmask_indices is None:
             mask_indices, unmask_indices = self.get_random_indices(batch_size)
@@ -230,36 +371,32 @@ class MaskedSinogramAutoencoder(Model):
         # building encoder layer
         self.enc_norm = norm(name=f'{name}_dec_norm')
 
-        self.encoder = Sequential(
-            [
-                TransformerEncoder(
-                    project_dim=enc_dim,
-                    num_heads=enc_heads,
-                    mlp_dim=enc_mlp_units,
-                    mlp_dropout=dropout,
-                    attention_dropout=dropout,
-                    activation=activation
-                ) for _ in range(self.enc_layers)
-            ]
-        )
+        self.encoder = [
+            TransformerEncoder(
+                project_dim=enc_dim,
+                num_heads=enc_heads,
+                mlp_dim=enc_mlp_units,
+                mlp_dropout=dropout,
+                attention_dropout=dropout,
+                activation=activation
+            ) for _ in range(self.enc_layers)
+        ]
 
         # building decoder layers
         self.dec_norm = norm(name=f'{name}_dec_norm')
 
         self.decoder_projection = Dense(dec_dim, name=f'{name}_dec_projection')
 
-        self.decoder = Sequential(
-            [
-                TransformerEncoder(
-                    project_dim=enc_dim,
-                    num_heads=enc_heads,
-                    mlp_dim=enc_mlp_units,
-                    mlp_dropout=dropout,
-                    attention_dropout=dropout,
-                    activation=activation
-                ) for _ in range(dec_layers)
-            ]
-        )
+        self.decoder = [
+            TransformerEncoder(
+                project_dim=dec_dim,
+                num_heads=dec_heads,
+                mlp_dim=dec_mlp_units,
+                mlp_dropout=dropout,
+                attention_dropout=dropout,
+                activation=activation
+            ) for _ in range(dec_layers)
+        ]
 
         self.output_projection = Dense(sinogram_width * sinogram_height, name=f'{name}_output_projection')
 
@@ -288,15 +425,20 @@ class MaskedSinogramAutoencoder(Model):
         ) = self.patch_encoder(patches)
 
         # Pass the unmasked patches to the encoder.
-        encoder_outputs = self.enc_norm(self.encoder(unmasked_embeddings))
+        encoder_outputs = unmasked_embeddings
+        for block in self.encoder:
+            encoder_outputs = block(encoder_outputs)
+
+        encoder_outputs = self.enc_norm(encoder_outputs)
 
         # Create the decoder inputs.
         encoder_outputs = encoder_outputs + unmasked_positions
         decoder_inputs = ops.concatenate([encoder_outputs, masked_embeddings], axis=1)
 
         # Decode the inputs.
-        decoder_outputs = self.dec_projection(decoder_inputs)
-        decoder_outputs = self.decoder(decoder_outputs)
+        decoder_outputs = self.decoder_projection(decoder_inputs)
+        for block in self.decoder:
+            decoder_outputs = block(decoder_outputs)
         decoder_outputs = self.dec_norm(decoder_outputs)
 
         decoder_outputs = self.output_projection(decoder_outputs)
@@ -379,3 +521,27 @@ class MaskedSinogramAutoencoder(Model):
         })
 
         return cfg
+
+
+if __name__ == "__main__":
+    import os
+
+    os.environ["KERAS_BACKEND"] = "tensorflow"
+
+    import keras_core
+
+    model = MaskedSinogramAutoencoder(
+        enc_layers=4,
+        dec_layers=1,
+        sinogram_width=513,
+        sinogram_height=1,
+        input_shape=(1024, 513, 1),
+        enc_dim=256,
+        enc_mlp_units=2048,
+        dec_dim=256,
+        dec_mlp_units=2048,
+        mask_ratio=0.75
+    )
+    model.compile(optimizer=keras_core.optimizers.AdamW(learning_rate=5e-5, weight_decay=1e-5), loss='mse')
+    model.call(keras_core.random.normal(shape=(1, 1024, 513, 1)))
+    model.summary()
