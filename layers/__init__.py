0b42ffa3a1b9e94ae6a5b4cb8e9712e247f7dc4e
jedlimlx <jedlimlx@outlook.com>
2023-09-23 23:38:59 +0800

    Remove old code imports from __init__.py file

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,6 +1,1 @@
-from layers.old_code.residual_block import ResidualBlock, ResidualStack
-from layers.old_code.dense_block import DenseBlock, TransitionBlock
-from layers.old_code.convnext_block import ConvNeXtBlock
-from layers.old_code.inverted_residual_block import MBConvBlock, FusedMBConvBlock
-
 from layers.transformer_blocks import Patches, PatchDecoder, PatchEncoder, TransformerEncoder, TransformerDecoder
